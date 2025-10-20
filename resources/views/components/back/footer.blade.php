fd2fb6ffd534bb9682b5df130316222bb63dbced
Miftahul Haq <ciftah12@gmail.com>
2025-10-20 23:12:12 +0700

    Update footer.blade.php

--- resources/views/components/back/footer.blade.php
+++ resources/views/components/back/footer.blade.php
@@ -4,21 +4,13 @@
       {{-- Footer right --}}
       <div class="col-lg-auto ms-lg-auto">
         <ul class="list-inline list-inline-dots mb-0">
-          <li class="list-inline-item"><a href="https://docs.tabler.io" target="_blank" class="link-secondary"
-              rel="noopener">Dokumentasi</a></li>
           <li class="list-inline-item">
             <a href="https://github.com/stiepemb/" target="_blank" class="link-secondary" rel="noopener">
-              Made with
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                class="icon text-pink icon-inline icon-4">
-                <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
-              </svg>
-              IT STIE Pembangunan
+              Made with <i class="ti ti-heart-filled me-1"></i> IT STIE Pembangunan
             </a>
           </li>
           <li class="list-inline-item">
-            <a href="./changelog.html" class="link-secondary" rel="noopener"> v1.4.0 </a>
+            <a href="./changelog.html" class="link-secondary" rel="noopener"> {{ env('APP_VERSION') }} </a>
           </li>
         </ul>
       </div>
@@ -30,7 +22,6 @@
             <a href="https://stie-pembangunan.ac.id/" class="link-secondary">STIE Pembangunan Tanjungpinang</a>.
             All rights reserved
           </li>
-
         </ul>
       </div>
     </div>
