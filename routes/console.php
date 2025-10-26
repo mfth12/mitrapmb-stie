eb131dc9a15e58e81c4edcbb1c0c4fccc5f71db6
Miftahul Haq <ciftah12@gmail.com>
2025-10-26 14:52:42 +0700

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 $password = env('DB_PASSWORD');
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// job whatsapp
+Schedule::command('queue:work --stop-when-empty --queue=whatsapp')
+    ->sendOutputTo(storage_path() . '/logs/whatsapp.log')
+    ->withoutOverlapping()
+    ->everyMinute();
+// --stop-when-empty
