cd78f1c1050e91820068e291d693d6fa269e28c2
Miftahul Haq <ciftah12@gmail.com>
2025-10-27 01:37:16 +0700

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -10,6 +10,10 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
+//job queue default seperti email, media library, rutin
+Schedule::command('queue:work --stop-when-empty --queue=default')
+    ->everyMinute();
+
 // job whatsapp
 Schedule::command('queue:work --stop-when-empty --queue=whatsapp')
     ->sendOutputTo(storage_path() . '/logs/whatsapp.log')
